8593bcef27bc92f768ac3c8db1df0b1811847733
Taiji Endo <taijiendo2@gmail.com>
2020-08-22 00:56:51 +1200

    added function to load copper conc data, changed docstring and variables for interpolation function

--- data_prep_functions.py
+++ data_prep_functions.py
@@ -55,9 +55,6 @@ def load_pressures():
         Notes:
         ------
         The file name of file containing the data is hard coded inside this function.
-
-        Forgotten how to load data from a file? Review datalab under Files/cm/
-        engsci233 on the ENGSCI263 Canvas page.
     '''
     #read data in from csv
     data=np.genfromtxt(fname="ac_p.csv",delimiter=',',skip_header=True)
@@ -67,22 +64,48 @@ def load_pressures():
 
     return t,P
 
-def interpolate_extraction(t,tv,qv):
-    ''' Return extraction parameter q for Onehunga Aquifer.
+def load_concs():
+    ''' Returns time and conc measurements from conc csv file.
+
+        Parameters:
+        -----------
+        none
+
+        Returns:
+        --------
+        t : array-like
+            Vector of times (years) at which measurements were taken.
+        C : array-like
+            Vector of conc measurements.
+
+        Notes:
+        ------
+        The file name of file containing the data is hard coded inside this function.
+    '''
+    #read data in from csv
+    data=np.genfromtxt(fname="ac_cu.csv",delimiter=',',skip_header=True)
+    #first col - time vals, second col - temp. vals
+    t=data[:,0]
+    C=data[:,1]
+
+    return t,C
+
+def interpolate_forcer_linker(t,tv,qv):
+    ''' Return forcing or linking variable q or p for Onehunga Aquifer.
 
         Parameters:
         -----------
         t : array-like
-            Vector of times at which to interpolate the extraction rate.
+            Vector of times at which to interpolate the variable.
         tv : array-like
-            Vector of times for which extraction rate is known.
+            Vector of times for which the variable is known.
         qv : array-like
-            Vector of measured extraction rates.
+            Vector of known values for variable.
 
         Returns:
         --------
         q : array-like
-            Extraction rate (megalitres/day) interpolated at t.
+            Extraction rate (megalitres/day) or pressure[MPa] interpolated at t.
 
         Notes:
         ------
@@ -109,8 +132,8 @@ def extraction_unit_convert(q_data):
         q : array-like
             Vector of extraction rates in kg/year
     '''
-    #Desnity of water extracted assumed to be 997 kg/m^3 
-    q=(q_data/365)*0.997
+    #Density of water extracted assumed to be 1000 kg/m^3 
+    q=(q_data/365)
 
     return q
 
@@ -132,6 +155,23 @@ def pressure_unit_convert(p_data):
 
     return p
 
+def conc_unit_convert(c_data):
+    ''' Convert copper concentration into mass fraction
+
+        Parameters:
+        -----------
+        c_data : array-like
+           Vector of conc values in mg/L
+
+        Returns:
+        --------
+        c : array-like
+            Vector of conc in mass fraction (as a fraction of the entire aquifer mass)
+    '''
+    #assume density of water 1000kg/m^3
+    c=(c_data)*(1.e-6)
+    
+    return c    
 
 
 
