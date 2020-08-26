2726b72b691f7718a3a69ad777e2a4245cc20a35
Taiji Endo <taijiendo2@gmail.com>
2020-08-26 19:59:10 +1200

    moved conc ode and IE functions into lpm_solve.py

--- lpm_solve.py
+++ lpm_solve.py
@@ -31,4 +31,47 @@ def solve_p_lpm(t,a,b,p0,p1):
         pm.append(pm[-1] + 0.5*(t1-t0)*(dpdt2+dpdt1))
     
     # interp onto requested times  
-    return np.interp(t, tp, pm)             
+    return np.interp(t, tp, pm)
+
+#define low pressure boundary conc
+def low_p_bound(c,p,p0):
+    if p>p0:
+        # contaminated water outflow happens when pressure greater inside aquifer than
+        # at low pressure boundary    
+        return c
+    else:
+        return 0
+
+# define derivative function
+def c_lpm(c,t,a,b,p0,p1,d,m0,csrc):                 # order of variables important
+    # interpolate pressure from solution (FOR TESTING - run this function in an environment where tp, pm defined)
+    tp, pm = load_p_lpm_solution()
+    p = np.interp(t,tp,pm)           
+    # find conc at low pressure boundary
+    cprime=low_p_bound(c,p,p0)
+    #find m0*dc/dt
+    mdcdt=-(b/a)*(p-p0)*(cprime-c)+(b/a)*(p-p1)*c-d*(p-((p0+p1)/2))*csrc
+    #compute derivative
+    return mdcdt/m0
+
+# implement an improved Euler step to solve the ODE
+def solve_c_lpm(t,a,b,p0,p1,d,m0,csrc):
+    #load conc data
+    tc,c = load_concs()
+    # convert to mass fraction
+    c = conc_unit_convert(c)
+    # initial value
+    cm = [c[0],]
+    # solve at conc steps                           
+    for t0,t1 in zip(tc[:-1],tc[1:]):
+        # predictor gradient
+        dcdt1 = c_lpm(cm[-1]-c[0], t0, a, b, p0, p1, d, m0, csrc)
+        # predictor step
+        cp = cm[-1] + dcdt1*(t1-t0)             
+        # corrector gradient
+        dcdt2 = c_lpm(cp-c[0], t1, a, b, p0, p1, d, m0, csrc)
+        # corrector step
+        cm.append(cm[-1] + 0.5*(t1-t0)*(dcdt2+dcdt1))
+    
+    #interp onto requested times
+    return np.interp(t, tc, cm)                   
--- solve_calibrate.py
+++ solve_calibrate.py
@@ -20,6 +20,17 @@ def pressure_lpm_model():
     ax.set_title('a={:2.1e},   b={:2.1e},   p0={:2.1e},  p1={:2.1e}'.format(*pars),size=14)
     plt.show()
 
+    #save the solution to a file to be read 
+    #store the solution in a single array
+    pressure_soln = np.column_stack((tp,pm))
+    #save the pressure solution to a .txt file
+    # opening a file
+    fp = open('p_lpm_soln.txt','w')
+    #write header
+    fp.write('t[year], P[MPa]\n')
+    #enter pressure solution data
+    [fp.write('{:f}, {:f}\n'.format(t,p)) for t, p in pressure_soln]  
+
     return pm, tp, pars
 
 def conc_lpm_model(pm,tp,pressure_pars):
@@ -29,48 +40,20 @@ def conc_lpm_model(pm,tp,pressure_pars):
     # load conc data and compute derivative
     tc,c = load_concs()
     c = conc_unit_convert(c)
-
-    #define low pressure boundary conc
-    def low_p_bound(c,p,p0):
-        if p>p0:
-            # contaminated water outflow happens when pressure greater inside aquifer than
-            # at low pressure boundary    
-            return c
-        else:
-            return 0
-
-    # define derivative function
-    def c_lpm(c,t,a,b,p0,p1,d,m0,csrc):                 # order of variables important
-        p = np.interp(t,tp,pm)           # interpolate pressure
-        #dqdti = np.interp(t,tq,dqdt)     # interpolate derivative
-        cprime=low_p_bound(c,p,p0)
-        #find m0*dc/dt
-        mdcdt=-(b/a)*(p-p0)*(cprime-c)+(b/a)*(p-p1)*c-d*(p-((p0+p1)/2))*csrc
-        #compute derivative
-        return mdcdt/m0
-
-    # implement an improved Euler step to solve the ODE
-    def solve_lpm(t,a,b,p0,p1,d,m0,csrc):
-        cm = [c[0],]                            # initial value
-        for t0,t1 in zip(tc[:-1],tc[1:]):           # solve at conc steps
-            dcdt1 = c_lpm(cm[-1]-c[0], t0, a, b, p0, p1, d, m0, csrc)   # predictor gradient
-            cp = cm[-1] + dcdt1*(t1-t0)             # predictor step
-            dcdt2 = c_lpm(cp-c[0], t1, a, b, p0, p1, d, m0, csrc)       # corrector gradient
-            cm.append(cm[-1] + 0.5*(t1-t0)*(dcdt2+dcdt1))  # corrector step
-        return np.interp(t, tc, cm)             # interp onto requested times
         
     # use CURVE_FIT to find "best" model
     from scipy.optimize import curve_fit
     #use parameter bounds to effectively fix params a,b,p0,p1
     e=np.finfo(float).eps
     #calibrate pressure model 
-    pars = curve_fit(solve_lpm, tc, c, [a, b, p0, p1, 0.5e9, 1.e10, 2.e-6], bounds=([a-e,b-e,p0-e,p1-e,-np.inf, -np.inf, -np.inf],[a,b,p0,p1,np.inf, np.inf, np.inf]))[0]
+    pars = curve_fit(solve_c_lpm, tc, c, [a, b, p0, p1, 0.5e9, 1.e10, 2.e-6], bounds=([a-e,b-e,p0-e,p1-e,-np.inf, -np.inf, -np.inf],[a,b,p0,p1,np.inf, np.inf, np.inf]))[0]
     pars = np.append(pressure_pars,pars[4:])
+    
     # plot the best solution
-    pm = solve_lpm(tp,*pars)
+    cm = solve_c_lpm(tp,*pars)
     f,ax = plt.subplots(1,1,figsize=(12,8))
     ax.plot(tc, c, 'ro', label = 'observations')
-    ax.plot(tp, pm, 'k-', label='model')
+    ax.plot(tp, cm, 'k-', label='model')
     ax.set_ylabel("concentration [mass fraction]",size=14); ax.set_xlabel("time[year]",size=14)
     ax.legend(prop={'size':14})
     ax.set_title('a={:2.1e},   b={:2.1e},   p0={:2.1e},  p1={:2.1e}, d={:2.1e}, m0={:2.1e}, csrc={:2.1e}'.format(*pars),size=14)
