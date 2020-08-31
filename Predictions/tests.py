aa31561235cea793b9d0b4a13f44b73c3a2d08cf
Taiji Endo <taijiendo2@gmail.com>
2020-09-01 00:11:29 +1200

    adapted functions, plots, files for generating preliminary predictions

---
+++ Predictions/tests.py
@@ -0,0 +1,50 @@
+import solve_calibrate as sc
+import data_prep_functions as dpf
+import numpy as np
+import lpm_solve as ls
+
+
+def test_extraction_unit_conversion():
+    #test basic conversion
+    try:
+        assert(dpf.extraction_unit_convert(1) - 2.739726027e-3 < 0.001)
+    except(AssertionError):
+        print("extraction_unit_conversion function from data prep functions aint right")
+
+    #test np arrays
+    try:
+        input = np.array([1, 1, 0, 20])
+        assert(np.linalg.norm(dpf.extraction_unit_convert(input) - [2.739726027e-3, 2.739726027e-3, 0, 0.05479452055]) < 0.001)
+    except(AssertionError):
+        print("extraction_unit_conversion function from data prep functions has problems with np arrays")      
+
+
+def test_conc_unit_convert():
+    #test individual input
+    try:
+        assert(dpf.conc_unit_convert(1) - 1.e-6 < 0.001)
+    except(AssertionError):
+        print("data_prep_functions conc_unit_convert function not right")
+    
+    #test array input
+    try:
+        input = np.array([1, 0, 2, 1])
+        assert(np.linalg.norm(dpf.conc_unit_convert(input) - [1.e-6, 0, 2.e-6, 1.e-6]) < 0.001)
+    except (AssertionError):
+        print("data_prep_functions conc_unit_convert function not right with array inputs")
+
+
+def test_p_lpm():
+    try:
+        assert(ls.p_lpm(0, 1, 2, 3, 4, 5)-30 < 0.001)
+        print("alg here")
+    except(AssertionError):
+        print("p_lpm isnt right")
+        
+def test_solve_p_lpm():
+    try: 
+        assert(ls.solve_p_lpm([1, 2, 3, 4, 5],1,2,20,40)-[0.03129011 0.03129011 0.03129011 0.03129011 0.03129011] < 0.001)
+        print("function appears to be working fine")
+    except(AssertionError):
+        print("something wrong with the pressure ODE numerical solver")
+        
